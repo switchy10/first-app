bab9b95152c5b042d2e0782fe012cf1e531ed0f0
Tyler Hixon <TylerHixon@tylers-air.attlocal.net>
2016-11-07 11:34:51 -0500

    Revert "no message"
    This reverts commit c91d6d7845a6438f4902e7439941040f1c369967.

--- stuff/stuff/ViewController.swift
+++ stuff/stuff/ViewController.swift
@@ -1,2 +1,47 @@
+//
+//  ViewController.swift
+//  stuff
+//
+//  Created by Tyler Hixon on 11/6/16.
+//  Copyright © 2016 Tyler Hixon. All rights reserved.
+//
 
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    @IBAction func buttontapped(_ sender: Any) {
+    
+       tapCount = tapCount + 1
+        print(tapCount)
+        if tapCount >= 20 {
+            theLabel.text = "You tapped the button 20 times!"
+        }
+    
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    @IBAction func theOtherButton(_ sender: Any) {
+        theLabel.text = "Why you press dis button??"
+        print("Button 2 tapped")
+    }
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    }
+
+}
 
